3662de385c30a8f115cbe808a53f24b69268bcf9
brightonchebor <cheborbrighton805@gmail.com>
2025-05-26 19:17:33 +0300

    cn

--- dummy/apps.py
+++ dummy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DummmyConfig(AppConfig):
+class DummyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "dummmy"
+    name = "dummy"
