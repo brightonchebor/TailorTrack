b182420ea2175f7fced2f280ff229798b7c49a4e
brightonchebor <cheborbrighton805@gmail.com>
2025-05-27 17:08:13 +0300

    cn

--- dummy/urls.py
+++ dummy/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
-    path('create_order/', create_order,name='create_order'),
+    path('create_order/', create_order, name='create_order'),
+    path('customer_list/', customer_list, name='customer_list'),
+    path('order_detail/', order_detail, name='order_detail'),
+    path('order_list/', order_list, name='order_list'),
+    path('payment_list/', payment_list, name='payment_list'),
 ]
