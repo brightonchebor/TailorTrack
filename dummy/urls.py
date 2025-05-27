8f4901ab9a0c953c48f783b8d63abbf10d3ad960
brightonchebor <cheborbrighton805@gmail.com>
2025-05-27 17:05:47 +0300

    cn

--- dummy/urls.py
+++ dummy/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', dashboard, name='dashboard'),
+    path('create_order/', create_order,name='create_order'),
 ]
--- dummy/views.py
+++ dummy/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def dashboard(request):
+
+    context = {}
+
+    return render(request, 'myapp/dashboard.html', context)
+
+def create_order(request):
+
+    context = {}
+
+    return render(request, 'myapp/create_order.html', context)
+
+def customer_list(request):
+
+    context = {}
+
+    return render(request, 'myapp/customer_list.html', context)
+
+def order_detail(request):
+
+    context = {}
+
+    return render(request, 'myapp/order_detail.html', context)
+
+def order_list(request):
 
     context = {}
 
     return render(request, 'myapp/order_list.html', context)
+
+def payment_list(request):
+
+    context = {}
+
+    return render(request, 'myapp/payment_list.html', context)
